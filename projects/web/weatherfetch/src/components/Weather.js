5d489fe80351069a37c6dc4ead99e8d01e702784
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-02-16 23:15:51 -0800

    added something

--- projects/web/weatherfetch/src/components/Weather.js
+++ projects/web/weatherfetch/src/components/Weather.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Geocode from 'react-geocode';
+import axios from 'axios';
 
 const Weather = () => {
     Geocode.setApiKey("AIzaSyCY4qx27h0FeRhO4XMUf-OpErdSZF3xuzM");
@@ -7,13 +8,14 @@ const Weather = () => {
     const [location, setLocation] = useState({
         lat: null,
         lng:null,
-        address: ''
+        address: '',
+        weather: 0
     })
 
     useEffect(()=> {
         if('geolocation' in navigator){
             navigator.geolocation.getCurrentPosition(pos => {
-                setLocation({
+                setLocation({...location,
                     lat: pos.coords.latitude,
                     lng: pos.coords.longitude
                 })
@@ -24,32 +26,42 @@ const Weather = () => {
         }
     }, [])
 
-    const handleClick = () => {
+    const handleClick = e => {
+        e.preventDefault();
         Geocode.fromAddress(location.address)
         .then(res => {
-            setLocation({
+            setLocation({...location,
                 lat: res.results[0].geometry.location.lat,
                 lng: res.results[0].geometry.location.lng
             })
             console.log(location)
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lng}&appid=14318e73194655c38ceca5222adc5301&units=imperial`)
+            .then(data =>{ 
+                console.log(data)
+                setLocation({...location,
+                    weather: Math.round(data.data.main.temp)
+                })
+            })
         })
     }
 
     const handleChange = e => {
-        setLocation({
+        setLocation({...location,
             [e.target.name] : e.target.value 
         })
     }
 
     return(
         <div>
-            <input
-              name="address"
-              onChange={handleChange}
-              value = {location.address}
-            />
-            <button onClick={handleClick}></button>
-            <div>{`latitude: ${location.lat}, longtitude: ${location.lng}`}</div>
+            <form>
+                <input
+                name="address"
+                onChange={handleChange}
+                value = {location.address}
+                />
+                <button type="submit" onClick={handleClick}>Get weather</button>
+            </form>
+            <div>{`latitude: ${location.lat}, longtitude: ${location.lng}, weather: ${location.weather}`}</div>
         </div>
 
     )
